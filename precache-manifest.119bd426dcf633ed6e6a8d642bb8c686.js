842f64429f4b202eef97a0b67f54630f5a441528
Ada Cheung <ada.cheung96@gmail.com>
2020-07-09 12:05:25 -0400

    Updates

---
+++ precache-manifest.119bd426dcf633ed6e6a8d642bb8c686.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6848e8c91beb87a74c3a62e5d145bea6",
+    "url": "/LetsEat-APP/index.html"
+  },
+  {
+    "revision": "a56f3981eda6f8b59bf7",
+    "url": "/LetsEat-APP/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "a56f3981eda6f8b59bf7",
+    "url": "/LetsEat-APP/static/js/2.c16af191.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/LetsEat-APP/static/js/2.c16af191.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6604e4bb2fe0dc9a91a5",
+    "url": "/LetsEat-APP/static/js/main.bfacad83.chunk.js"
+  },
+  {
+    "revision": "0ad395d13115ec40b7b8",
+    "url": "/LetsEat-APP/static/js/runtime-main.14a15f01.js"
+  }
+]);
